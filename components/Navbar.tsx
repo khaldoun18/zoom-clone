3931a8f972a5eb836792afca45479997e219e16d
khaldoun18 <84587554+khaldoun18@users.noreply.github.com>
2024-05-15 16:59:42 +0300

    added the clerk auth

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,25 +2,31 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import MobileNav from './MobileNav'
+import { SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
 
 const Navbar = () => {
   return (
-   <nav className='flex-between fixed z-50 w-full bg-dark-1 pz-6 py-4 lg:px-10'>
-    <Link
-    href='/' className='flex items-center gap-1'>
-    <Image
-    src="/icons/logo.svg"
-    alt='Yoom Logo'
-    width={32}
-    height={32}
-    className='max-sm:size-10'/>
-    <p className='text-[26px] font-extrabold text-white max-sm:hidden'>Yoom</p>
-    </Link>
-    <div className='flex-between gap-5'>
-    {/* clerk user management */}
-    <MobileNav/>
-    </div>
-   </nav>
+    <nav className='flex-between fixed z-50 w-full bg-dark-1 pz-6 py-4 lg:px-10'>
+      <Link
+        href='/' className='flex items-center gap-1'>
+        <Image
+          src="/icons/logo.svg"
+          alt='Yoom Logo'
+          width={32}
+          height={32}
+          className='max-sm:size-10' />
+        <p className='text-[26px] font-extrabold text-white max-sm:hidden'>Yoom</p>
+      </Link>
+      <div className='flex-between gap-5'>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+        {/* <SignedOut>
+          <SignInButton />
+        </SignedOut> */}
+        <MobileNav />
+      </div>
+    </nav>
   )
 }
 
