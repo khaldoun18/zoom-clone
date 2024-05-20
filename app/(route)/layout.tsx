4eac85f42556e012dc026a860c4c5918d97a98e5
khaldoun18 <84587554+khaldoun18@users.noreply.github.com>
2024-05-20 18:22:54 +0300

    added features for the meeting room

--- app/(route)/layout.tsx
+++ app/(route)/layout.tsx
@@ -1,6 +1,15 @@
 import StreamVideoProvider from '@/providers/StreamClientProvider'
+import { Metadata } from 'next';
 import React, { ReactNode } from 'react'
 
+
+export const metadata: Metadata = {
+  title: "Zoom Clone",
+  description: "Video Calling app",
+  icons:{
+    icon:'/icons/logo.svg'
+  }
+};
 const RootLayout = ({children}: {children: ReactNode}) => {
   return (
     <main>
