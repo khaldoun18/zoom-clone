824e88fe61ba9667f2ba89a4719c0b9783703ceb
khaldoun18 <84587554+khaldoun18@users.noreply.github.com>
2024-05-21 14:28:10 +0300

    added personal meeting features

--- components/MeetingtypeList.tsx
+++ components/MeetingtypeList.tsx
@@ -9,6 +9,7 @@ import { Call, useStreamVideoClient } from '@stream-io/video-react-sdk'
 import { useToast } from "@/components/ui/use-toast"
 import { Textarea } from './ui/textarea'
 import ReactDatePicker from 'react-datepicker'
+import { Input } from './ui/input'
 
 const MeetingtypeList = () => {
     const router = useRouter()
@@ -143,6 +144,19 @@ const MeetingtypeList = () => {
                 className="text-center"
                 buttonText="Start Meeting"
                 handleClick={createMeeting} />
+
+            <MeetingModal
+                isOpen={meetingState === 'isJoiningMeeting'}
+                onClose={() => setMeetingState(undefined)}
+                title="Type the link here"
+                className="text-center"
+                buttonText="Join Meeting"
+                handleClick={()=>router.push(values.link)} >
+                    <Input
+                    placeholder='Meeting Link'
+                    className='border-none bg-dark-3 focus-visible:ring-0 focus-visible:ring-offset-0'
+                    onChange={(e)=>setValues({...values,link:e.target.value})}/>
+                </MeetingModal>
         </section>
     )
 }
