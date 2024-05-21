0bc5581e337cc7100e75c54463f1225590acf560
khaldoun18 <84587554+khaldoun18@users.noreply.github.com>
2024-05-21 12:58:00 +0300

    added recorded page features

--- hooks/useGetCalls.ts
+++ hooks/useGetCalls.ts
@@ -46,6 +46,6 @@ export const useGetCalls=()=>{
 
 
     return {
-        endedCalls,upcomingCalls,recordings:calls,isLoading
+        endedCalls,upcomingCalls,callRecordings:calls,isLoading
     }
 }
